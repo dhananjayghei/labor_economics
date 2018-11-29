e0feffa20bebdf7cd935141fe5b50adc77b0d322
dhananjay <djghei@gmail.com>
2018-11-28 21:38:44 -0600

    Updates to the homework.

--- src/burdett_mortensen.R
+++ src/burdett_mortensen.R
@@ -9,6 +9,28 @@ bhps <- read.csv("../data/bm_data.csv", header=TRUE)
 sex_ratio <- round(prop.table(table(bhps$sex))*100, digits=2)
 names(sex_ratio) <- c("Female", "Male")
 
+# Sample unemployment rate
+# Initial spell unemployment rate
+# NOT sure if this is the correct way to measure unemployment rate
+# in the sample.
+# This calculates the unemployment rate only in the initial spell. 
+unemp_overall <- bhps %>%
+    summarise(unemp = length(e1[which(e1==0)]),
+              unemp_rate = unemp*100/nrow(.)) %>% data.frame()
+
+# Sample unemployment rate by sex
+# Initial spell unemployment rate
+unemp_sex <- bhps %>%
+    group_by(sex) %>%
+    summarise(unemp = length(e1[which(e1==0)]),
+              unemp_rate = unemp*100/nrow(.)) %>% data.frame()
+
+# Sample unemployment rate by education
+unemp_educ <- bhps %>%
+    group_by(educ) %>%
+    summarise(unemp = length(e1[which(e1==0)]),
+              unemp_rate = unemp*100/nrow(.)) %>% data.frame()
+
 # Proportion of people with right censoring
 # For initially employed people (e1=1)
 Rcens_job <- bhps %>%
@@ -26,19 +48,40 @@ Rcens_unemp <- bhps %>%
     mutate(Rcens_unemp = 1 - (u2jT/total)) %>% data.frame()
 
 
-# Plotting the Empirical CDF of log wages in 1
+# Plotting the Empirical CDF G in spell 1 (Gcdf is also a function)
 Gcdf <- ecdf(bhps$logw1)
-# Plotting the kernel density of log wages in 1
+# Plotting the kernel density of log wages in spell 1
 Gdens <- density(bhps$logw1, na.rm=TRUE)
+# Constructing the density function using linear interpolation
+Gdensity <- approxfun(Gdens)
 # Create a variable categorizing logw1 into 25 bins
-logw1_qtls <- quantile(bhps$logw1, probs=seq(0,1,1/25), na.rm=TRUE)
+bhps$logw1_qtls <- cut(bhps$logw1, quantile(bhps$logw1, probs=seq(0,1,1/25), na.rm=TRUE))
+#Getting the values of the quantiles
+logw1_qtls <- as.numeric(unlist(strsplit(unlist(strsplit(names(table(bhps$logw1_qtls)), split="[,]"))[seq(2,50,by=2)], split="]")))
+# Changing the naming to Q's for easy readability
+bhps$logw1_qtls <- cut(bhps$logw1, quantile(bhps$logw1, probs=seq(0,1,1/25), na.rm=TRUE), labels=paste0("Q", 1:25))
 # Calculate the mean spell duration in each of the quantiles
-avg_spelldur1 <- do.call(rbind, lapply(logw1_qtls, function(x){
-    avg_spelldur1 <- mean(bhps[which(bhps$logw1 <= x), "spelldur1"], na.rm=TRUE)
-    return(avg_spelldur1)
-}))
-# Binding the qunatiles with the mean spell duraation
-dur_wages <- data.frame(logw1_qtls=logw1_qtls, avg_spelldur1=avg_spelldur1)
-plot(dur_wages$logw1_qtls, dur_wages$avg_spelldur1, type="l")
+avg_spelldur1 <- bhps %>%
+    group_by(logw1_qtls) %>%
+    summarise(avg_spelldur1 = mean(spelldur1, na.rm=TRUE)) %>%
+    data.frame() %>%
+    filter(!is.na(logw1_qtls))
+# Binding the quantiles with the mean spell duraation
+dur_wages <- data.frame(wage1_bins=logw1_qtls, avg_spelldur1=avg_spelldur1)
 
+# Construct the CDF F (Fcdf is also a function)
+Fcdf <- ecdf(bhps[which(bhps$e1==0 & bhps$u2j==1), "logw2"])
+# Constructing the estimate of f
+Fdens <- density(bhps[which(bhps$e1==0 & bhps$u2j==1), "logw2"], na.rm=TRUE)
+# Constructing the density function using linear interpolation
+Fdensity <- approxfun(Fdens)
 
+# Estimating kappa_1 non-paramterically
+kappa1 <- function(x){
+    kappa1 <- (Fcdf(x)-Gcdf(x))/(Gcdf(x)*(1-Fcdf(x)))
+    return(kappa1)
+}
+
+wages1 <- unique(bhps$logw1)
+kappa_1 <- do.call(rbind, lapply(wages1, kappa1))
+kappa_1 <- kappa_1[is.finite(kappa_1)]
--- src/burdett_mortensen_plots.R
+++ src/burdett_mortensen_plots.R
@@ -8,3 +8,13 @@ plot(Gcdf, main="", xlab="Log(wages)", ylab="Empirical CDF")
 
 # Plot for the density of the empirical CDF
 plot(Gdens, main="", xlab="", ylab="Density")
+
+# Plot the mean durations against the wage percentiles
+plot(dur_wages$logw1_qtls, dur_wages$avg_spelldur1, type="l",
+     main="", xlab="Log(wages) in Quantiles",
+     ylab="Mean duration of initial spell", lwd=2)
+
+# Plot of estimated F vs estimated G (CDF)
+plot.stepfun(Fcdf, do.points=FALSE, main="", xlab="Log(wages)", ylab="Cumulative Distribution Function", col="black", lwd=2)
+lines(Gcdf, col="gray50", lwd=2)
+legend("topleft", legend=c("Predicted F", "Predicted G"), col=c("black", "gray50"), lty=rep(1,2), lwd=rep(2,2), bty="n")
