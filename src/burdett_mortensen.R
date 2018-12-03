dd3c8a4909fab8823893701ef9ac38b1717136dc
dhananjay <djghei@gmail.com>
2018-12-02 21:36:20 -0600

    Adding MLE estimation for data and the simulated data. Adding plots in the main file itself.

--- src/burdett_mortensen.R
+++ src/burdett_mortensen.R
@@ -1,6 +1,8 @@
+# Including the functions file
+source("functions.R")
 # Loading in the required libraries
 library(dplyr)
-
+library(xtable)
 # Reading in the data
 bhps <- read.csv("../data/bm_data.csv", header=TRUE)
 
@@ -77,7 +79,17 @@ avg_spelldur1 <- bhps %>%
     filter(!is.na(logw1_qtls))
 # Binding the quantiles with the mean spell duraation
 dur_wages <- data.frame(wage1_bins=logw1_qtls, avg_spelldur1=avg_spelldur1)
+colnames(dur_wages) <- c("wage", "qtls", "avg_spell")
+
+pdf(file="../doc/pics/duration_wages.pdf", width=5.4, height=3.8)
+par(mai=c(.8,.8,.3,.2))
+plot(dur_wages$wage, dur_wages$avg_spell, main="", xlab="Log(wages)", ylab="Mean Spell Duration", col="black",
+     type="l", lwd=2)
+dev.off()
+
 names(dur_wages) <- c("Wage (Bins)", "Quartiles", "Avg. Spell Duration")
+dur_wages <- dur_wages[, -1]
+genxtable(xtable(dur_wages, align="lrr"), basename="duration_wages", include.rownames=FALSE)
 
 # Construct the CDF F (Fcdf is also a function)
 Fcdf <- ecdf(bhps[which(bhps$e1==0 & bhps$u2j==1), "logw2"])
@@ -86,7 +98,26 @@ Fdens <- density(bhps[which(bhps$e1==0 & bhps$u2j==1), "logw2"], na.rm=TRUE)
 # Constructing the density function using linear interpolation
 Fdensity <- approxfun(Fdens)
 
-# Estimating kappa_1 non-paramterically
+# Plot G vs F
+pdf(file="../doc/pics/FG_data.pdf", width=5.4, height=3.8)
+par(mai=c(.8,.8,.3,.2))
+plot(Gcdf, main="", xlab="Log(wages)", ylab="Cumulative Distribution Function", col="black")
+plot.stepfun(Fcdf, do.points=FALSE, add=TRUE, col="gray45")
+legend("topleft", legend=c("G(w)", "F(w)"), lty=rep(1,2), lwd=rep(1,2), col=c("black", "gray45"),
+       bty="n")
+dev.off()
+
+# Plot g vs f
+pdf(file="../doc/pics/FG_density.pdf", width=5.4, height=3.8)
+par(mai=c(.8,.8,.3,.2))
+plot(Gdens, main="", xlab="Log(wages)", ylab="Density", col="black", xlim=c(min(Fdens$x), max(Fdens$x)))
+lines(Fdens, col="gray45")
+legend("topleft", legend=c("G(w)", "F(w)"), lty=rep(1,2), lwd=rep(1,2), col=c("black", "gray45"),
+       bty="n")
+dev.off()
+
+
+# Estimating kappa_1 non-parametrically
 kappa1 <- function(x){
     kappa1 <- (Fcdf(x)-Gcdf(x))/(Gcdf(x)*(1-Fcdf(x)))
     return(kappa1)
@@ -96,93 +127,120 @@ wages1 <- unique(bhps$logw1)
 kappa_1 <- do.call(rbind, lapply(wages1, kappa1))
 kappa_1 <- kappa_1[is.finite(kappa_1)]
 
-## # First transition for employed (v - will be used for MLE)
-## bhps$v <- NA
-## bhps[which(bhps$e1==1), "v"] <- 0
-## bhps[which(bhps$e1==1 & bhps$j2u==1), "v"] <- 1
+plot(kappa_1, type="l")
+
 
 # Starting the MLE estimation
-# Writing the CDF as a function of kappa_1
-# Page 327 (Bontemps, Robin, Van Den Berg - IER (2000))
-F <- function(w, theta){
-    lambda0 <- theta[1]
-    lambda1 <- theta[2]
-    delta <- theta[3]
-    kappa <- lambda1/delta
-    F <- (1+kappa)*Gcdf(w)/(1+kappa*Gcdf(w))
-    return(F)
-}
-# Writing the PDF of F as a function of kappa_1
-# Page 328 (Bontemps, Robin, Van Den Berg - IER (2000))
-f <- function(w, theta){
-    lambda0 <- theta[1]
-    lambda1 <- theta[2]
-    delta <- theta[3]
-    kappa <- lambda1/delta
-    f <- (1+kappa)*Gdensity(w)/((1+kappa*Gcdf(w))^2)
-    return(f)
-}
+# Storing the data set for MLE
+dat <- bhps[, c("e1", "spelldur1", "logw1", "j2u", "j2j",
+                "logw2", "right_censor")]
 
-# Likelihood for unemployed people
-L0 <- function(theta, cu, du, xu){
-    lambda0 <- theta[1]
-    lambda1 <- theta[2]                 
-    delta <- theta[3]
-    scale <- lambda0/(lambda0+delta)
-    dens_spell_dur <- (1-cu)*log(lambda0 * exp(-lambda0*du))
-    dens_accept_job <- (1-cu)*log(f(xu, theta))
-    logLL_unemp <- scale*sum(dens_spell_dur+dens_accept_job, na.rm=TRUE)
-    return(-logLL_unemp)
-}
-
-cu <- bhps[which(bhps$e1==0), "right_censor"]
-du <- bhps[which(bhps$e1==0), "spelldur1"]
-xu <- bhps[which(bhps$e1==0), "logw2"]
-eu <- bhps[which(bhps$e1==0), "e1"]
-
-L1 <- function(theta, ce, de, j2j, j2u, xe){
-    lambda0 <- theta[1]
-    lambda1 <- theta[2]
-    delta <- theta[3]
-    scale <- delta/(lambda0+delta)
-    # Density of job value
-    dens_job_val <- log(Gdensity(xe))
-    # Density of spell duration
-    dens_spell_dur <- (1-ce)*log(delta+lambda1*(1-F(xe, theta)))-
-        (delta+lambda1*(1-F(xe, theta)))*de
-    # Conditional probability of cause of job spell termination
-    prob_job_trans <- j2u*log(delta/(delta+lambda1*(1-F(xe, theta))))+ j2j*log((lambda1*(1-F(xe, theta)))/(delta+ lambda1*(1-F(xe, theta)))) 
-    logLL_emp <- scale*sum(dens_job_val+dens_spell_dur+prob_job_trans,
-                     na.rm=TRUE)
-    return(-logLL_emp)
-}
-
-ce <- bhps[which(bhps$e1==1), "right_censor"]
-de <- bhps[which(bhps$e1==1), "spelldur1"]
-j2j <- bhps[which(bhps$e1==1), "j2j"]
-j2u <- bhps[which(bhps$e1==1), "j2u"]
-xe <- bhps[which(bhps$e1==1), "logw1"]
-ee <- bhps[which(bhps$e1==1), "e1"]
-    
-LLfull <- function(theta, cu, du, xu, ce, de, j2j, j2u, xe){
-    lambda0 <- theta[1]
-    lambda1 <- theta[2]
-    delta <- theta[3]
-    full_logLL <- L0(theta=theta, cu=cu, du=du,
-                                               xu=xu)+
-        L1(theta=theta, ce=ce, de=de, j2j=j2j,
-                                   j2u=j2u, xe=xe)
-    
-    return(full_logLL)
-}
-
-mle.dat <- optim(par=c(.1,.2,.3), fn=LLfull, cu=cu, du=du, xu=xu,
-      ce=ce, de=de, j2j=j2j, j2u=j2u, xe=xe, method="L-BFGS-B",
-      lower=c(rep(0.01,3)), upper=1, hessian=TRUE)
-
-# Getting the standard errors
+# Maximum Likelihood Estimation
+mle.dat <- optim(par=c(1,2,3), full_likelihood, dat=dat, method="L-BFGS-B",
+                 lower=c(0.01,.01,.01), upper=c(1,1,1), hessian=TRUE)
+# Get the standard errors
+# Instead of bootstrapping, I am using the inverse of Hessian (Fisher information matrix)
+# to compute the asymptotic standard errors. This gives a lower bound on the variance.
 se.estimates <- sqrt(diag(mle.dat$hessian^(-1)))
 
+# Sanity checks for the model - Estimating the coefficients/ distribution function
+# Calculate the unemployment rate from the estimates
+unemp_est <- mle.dat$par[3]/(mle.dat$par[3]+mle.dat$par[1])
+
+# Estimate F from kappa_1
+pdf(file="../doc/pics/estF_data.pdf", width=5.4, height=3.8)
+par(mai=c(.8,.8,.3,.2))
+plot(sort(dat$logw1), sort(F(dat$logw1, theta=mle.dat$par)), type="l",
+     main="", xlab="Log(wages)", ylab="Cumulative Distribution Function", col="black")
+plot.stepfun(Fcdf, add=TRUE, do.points=FALSE, col="gray45")
+legend("topleft", legend=c("Empirical F", "Predicted F (using MLE)"),
+       lty=rep(1,2), lwd=rep(1,2), col=c("black", "gray45"), bty="n")
+dev.off()
 
 # Simulated data
 sim.dat <- read.csv("../data/bm_data_simulated.csv")
+colnames(sim.dat)[1] <- "e1"
+
+# Generating the right censor variable
+sim.dat$right_censor <- 1
+sim.dat[which(sim.dat$e1==1 & (sim.dat$j2j==1 | sim.dat$j2u==1)), "right_censor"] <- 0
+sim.dat[which(sim.dat$e1==0 & sim.dat$u2j==1), "right_censor"] <- 0
+
+# Changing the log wages for unemployed to NA
+sim.dat[which(sim.dat$e1==0), "logw1"] <- NA
+# Generating the Empirical CDF function
+Gsim <- ecdf(sim.dat$logw1)
+# Generating the PDF
+Gdens <- density(sim.dat$logw1, na.rm=TRUE)
+Gdensity <- approxfun(Gdens)
+Gcdf <- Gsim
+likelihood_unemp_sim <- function(theta, dat){
+    x <- dat[which(dat$e1==0), ]
+    lambda0 <- theta[1]
+    lambda1 <- theta[2]
+    delta <- theta[3]
+    scale <- log(delta/(lambda0+delta))
+    dens_spell_dur <- log(lambda0^(1-x$right_censor) *
+                          exp(-lambda0*x$spelldur1))
+    # Integrating out the values for unobserved wage 2 (Slide 14 - Topic 2 from Jeremy's slides)
+#    dens_accept_job <- (1-x$right_censor)*log(f(x$logw2, theta))
+    logLL_unemp <- sum(scale+dens_spell_dur, na.rm=TRUE)
+    return(-logLL_unemp)
+}
+
+likelihood_sim <- function(theta, dat){
+    lambda0 <- theta[1]
+    lambda1 <- theta[2]
+    delta <- theta[3]
+    full_logLL <- likelihood_unemp_sim(theta=theta, dat=dat) +
+        likelihood_emp(theta=theta, dat=dat)
+    return(full_logLL)    
+}
+
+# Maximum Likelihood Estimation on the simulated data
+mle.sim <- optim(par=c(.1,.05,.01), likelihood_sim, dat=sim.dat, method="L-BFGS-B",
+                 lower=c(0.001,.001,.001), upper=c(1,1,1), hessian=TRUE)
+se.estimates.sim <- sqrt(diag(mle.sim$hessian^(-1)))
+
+# Calculate the unemployment rate from the estimates
+unemp_est_sim <- mle.sim$par[3]/(mle.sim$par[3]+mle.sim$par[1])
+# Unemployment rate in the sample
+unemp_sim <- data.frame(prop.table(table(sim.dat$e1)))
+# Estimating kappa_1 (The true kappa1 = 5)
+kappa1_sim <- mle.sim$par[2]/mle.sim$par[3]
+
+
+# Plot g vs f for simulated data
+pdf(file="../doc/pics/FG_density_sim.pdf", width=5.4, height=3.8)
+par(mai=c(.8,.8,.3,.2))
+plot(Gdens, main="", xlab="Log(wages)", ylab="Density", col="black", xlim=c(min(Fdens$x), max(Gdens$x)))
+lines(Fdens, col="gray45")
+legend("topleft", legend=c("G(w)", "F(w)"), lty=rep(1,2), lwd=rep(1,2), col=c("black", "gray45"),
+       bty="n")
+dev.off()
+
+# Construct the distribution of firm productivity
+productivity <- function(w, kappa){
+    p <- w + ((1+kappa*Gcdf(w))/(2*kappa*Gdensity(w)))
+    return(p)
+}
+
+firm.prod <- data.frame(do.call(rbind, lapply(sim.dat$logw1, function(x) productivity(x, kappa1_sim))))
+colnames(firm.prod) <- "firm_prod"
+sim.dat$firm.prod <- firm.prod$firm_prod
+
+sim.datPlot <- sim.dat[complete.cases(sim.dat$logw1, sim.dat$firm.prod), ]
+
+plot(x=sim.datPlot$logw1, y=sim.datPlot$firm.prod, main="", xlab="Log(wages)", ylab="Log(productivity)")
+
+# Construct the distribution of predicted profit rate
+profit <- function(w, kappa){
+    profit <- (productivity(w, kappa) - exp(w))/productivity(w, kappa)
+    return(profit)
+}
+
+profit.rate <- data.frame(do.call(rbind, lapply(sim.dat$logw1, function(x) productivity(x, kappa1_sim))))
+colnames(profit.rate) <- "profit_rate"
+sim.dat$profit.rate <- profit.rate$profit_rate
+
+plot(x=sim.dat$logw1, y=sim.dat$profit.rate, main="", xlab="Log(wages)", ylab="Profit rate")
